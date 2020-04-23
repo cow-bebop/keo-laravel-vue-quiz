65c83a2bb1da3e0c0e75c7e8de7f0dd4942de9f9
けお <o.kengo1987@gmail.com>
2020-04-23 20:30:10 +0900

    クイズ機能の実装

--- resources/js/components/page/Home.vue
+++ resources/js/components/page/Home.vue
@@ -17,23 +17,9 @@
               <img class="home-quiz__setting-h2-logo" src="/images/directory-icon.png" />出題設定
             </h2>
             <form>
-              <label>
-                <input type="checkbox" v-model="categories" value="1" checked />ビジネスマナー
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="2" />一般常識
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="3" />就職・転職
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="4" />法律
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="5" />IT
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="6" />雑学
+              <label v-for="(cate, index) in category" :key="index">
+                <input type="checkbox" v-model="categories" :value="cate.id" />
+                {{cate.name}}
               </label>
               <div class>
                 全項目チェック
@@ -67,9 +53,9 @@
             <h2 class="home__notice-h2">
               <img class="home__notice-h2-logo" src="/images/news-icon.png" />お知らせ情報
             </h2>
-            <dl>
-              <dt>2019/08/23</dt>
-              <dd>サイトを開設しました。</dd>
+            <dl v-for="(info, index) in information" :key="index">
+              <dt>{{info.created_at}}</dt>
+              <dd>{{info.information}}</dd>
             </dl>
           </section>
         </article>
@@ -96,8 +82,19 @@ export default {
   },
   data() {
     return {
-      categories: [1]
+      categories: [1],
+      information: [],
+      category: []
     };
+  },
+  mounted() {
+    this.$http.get("/api/category").then(response => {
+      this.category = response.data;
+    });
+
+    this.$http.get("/api/information").then(response => {
+      this.information = response.data;
+    });
   },
   methods: {
     goQuiz() {
